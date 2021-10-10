1770f90461105a4cad7022f1e97d5f0f87f1a2dc
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2021-10-10 11:00:37 +0300

    fix local variales@

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module dowmload_concur
+module github.com/evgeniygazetdinov/concurent_download_golang
 
 go 1.16
 
 require (
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/youtube-videos/go-youtube-dl v0.0.0-20170720135957-9ca8bafc4268 // indirect
 )
---
+++ src/lib/server_handler.go
@@ -0,0 +1,35 @@
+package lib
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"fmt"
+)
+
+func succes_handler(result string, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"Value": result, "Status": "true", "Err": ""})
+}
+
+func get_variable_string_from_uri(variable_name string, w http.ResponseWriter, r *http.Request) []string {
+	variable, ok := r.URL.Query()[variable_name]
+	if !ok || len(variable[0]) < 1 {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "missed"}`))
+	}
+	return variable
+}
+
+func get_integers_for_equalize(one_name string, second_name string, w http.ResponseWriter, r *http.Request) (int, int) {
+
+	one := get_variable_string_from_uri(one_name, w, r)
+	two := get_variable_string_from_uri(second_name, w, r)
+	first_int, err := strconv.Atoi(one[0])
+	second_int, err := strconv.Atoi(two[0])
+	if err != nil {
+		fmt.Println((err))
+	}
+	return first_int, second_int
+}
+
