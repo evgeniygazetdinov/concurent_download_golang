0a1be6f0d72a3233df0709e8cfceb65ab9590dba
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2021-10-11 07:28:20 +0300

    add python stuff

--- install_local_dep.sh
+++
@@ -1,4 +0,0 @@
-eval "GO111MODULE=auto \
-    CGO_ENABLED=0 \
-    GOOS=linux \
-    GOARCH=amd64: && go mod init dowmload_concur && go get github.com/gorilla/mux && go get github.com/gorilla/handlers && go get github.com/youtube-videos/go-youtube-dl";
