d2c5a47341cf224dd80886cd3961947c327eb19c
ev <zplacesound@gmail.com>
2024-05-24 10:05:28 +0300

    additional struct

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -37,180 +33,11 @@ type Message struct {
 	ChatId int
 }
 
-func CreateChat(db *sql.DB, usersId string) error {
-	query := "INSERT INTO chat (users_ids) VALUES (?)"
-	_, err := db.Exec(query, usersId)
-	if err != nil {
-		return err
-	}
-	fmt.Println("here")
-	return nil
-}
-
-func createChatHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	var chat Chat
-	json.NewDecoder(r.Body).Decode(&chat)
-
-	err = CreateChat(db, chat.UsersIds)
-	fmt.Println(err)
-	if err != nil {
-		http.Error(w, "Failed to create сhat", http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintln(w, "сhat created successfully")
-}
-
-func CreateUser(db *sql.DB, name, email string) error {
-	query := "INSERT INTO users (name, email) VALUES (?, ?)"
-	_, err := db.Exec(query, name, email)
-	fmt.Println(name, email)
-	if err != nil {
-		return err
-	}
-	fmt.Println("here")
-	return nil
-}
-
-func createUserHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	var user User
-	json.NewDecoder(r.Body).Decode(&user)
-
-	err = CreateUser(db, user.Name, user.Email)
-	fmt.Println(err)
-	if err != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintln(w, "User created successfully")
-}
-
-func getUserHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	// Get the 'id' parameter from the URL
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-
-	// Convert 'id' to an integer
-	userID, err := strconv.Atoi(idStr)
-
-	// Call the GetUser function to fetch the user data from the database
-	user, err := GetUser(db, userID)
-	if err != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	// Convert the user object to JSON and send it in the response
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
-}
-
-func GetUser(db *sql.DB, id int) (*User, error) {
-	query := "SELECT * FROM users WHERE id = ?"
-	row := db.QueryRow(query, id)
-
-	user := &User{}
-	err := row.Scan(&user.ID, &user.Name, &user.Email)
-	if err != nil {
-		return nil, err
-	}
-	return user, nil
-}
-
-func UpdateUser(db *sql.DB, id int, name, email string) error {
-	query := "UPDATE users SET name = ?, email = ? WHERE id = ?"
-	_, err := db.Exec(query, name, email, id)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func updateUserHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
-	// Get the 'id' parameter from the URL
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-
-	// Convert 'id' to an integer
-	userID, err := strconv.Atoi(idStr)
-
-	var user User
-	err = json.NewDecoder(r.Body).Decode(&user)
-
-	// Call the GetUser function to fetch the user data from the database
-	UpdateUser(db, userID, user.Name, user.Email)
-	if err != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintln(w, "User updated successfully")
-}
-
-func DeleteUser(db *sql.DB, id int) error {
-	query := "DELETE FROM users WHERE id = ?"
-	_, err := db.Exec(query, id)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	// Get the 'id' parameter from the URL
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-
-	// Convert 'id' to an integer
-	userID, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid 'id' parameter", http.StatusBadRequest)
-		return
-	}
-
-	user := DeleteUser(db, userID)
-	if err != nil {
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintln(w, "User deleted successfully")
-
-	// Convert the user object to JSON and send it in the response
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
+type Like struct {
+	ID      int
+	Owner   int
+	UserId  int
+	Message string
 }
 
 // @title           Swagger Example API
@@ -241,6 +68,7 @@ func main() {
 	r.HandleFunc("/api/user/{id}", getUserHandler).Methods("GET")
 	r.HandleFunc("/api/user/{id}", updateUserHandler).Methods("PUT")
 	r.HandleFunc("/api/user/{id}", deleteUserHandler).Methods("DELETE")
+	r.HandleFunc("/api/user/setLike/{id}/{id}", deleteUserHandler).Methods("DELETE")
 
 	r.HandleFunc("/api/chat", createChatHandler).Methods("POST")
 
