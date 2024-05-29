e39393e6725a91d5676c24170681815bdaf5642e
ev <zplacesound@gmail.com>
2024-05-29 10:15:03 +0300

    new example

---
+++ examples/install.sh
@@ -0,0 +1,2 @@
+sudo apt install delve && go install github.com/go-delve/delve/cmd/dlv@latest;
+sudo apt install delve && go install github.com/go-delve/delve/cmd/dlv@latest;
--- examples/main.go
+++ examples/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	counts := make(map[string]int)
-	input := bufio.NewScanner((os.Stdin))
-	counter := 0
-	for input.Scan() {
-		counts[input.Text()]++
-		counter++
-		if counter == 5 {
-			break
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println(os.Stderr, "dup %v\n", err)
+			continue
 		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Println("%d\t%s\n", n, line)
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
 }
